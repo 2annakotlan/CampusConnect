49aaa1fe15ae15b81a0ec8ba837b05a75bbd9ad4
Anna Kotlan <2annakotlan@gmail.com>
2026-05-25 16:29:57 -0400

    Update custom_css_file.py

--- custom_css_file.py
+++ custom_css_file.py
@@ -28,7 +28,7 @@ def get_custom_css_page(alignment="center", button_span="full"):
         
         /* Title style */
         h1 {{
-            color: #4A90E2;
+            color: #A7D8F0;
             font-family: 'Arial', sans-serif;
             font-weight: bold;
             margin-top: 20px;
@@ -43,7 +43,7 @@ def get_custom_css_page(alignment="center", button_span="full"):
         /* Button style */
         .stButton button {{
             width: {button_width};
-            background-color: #4A90E2;
+            background-color: #A7D8F0;
             color: white;
             font-size: 16px;
             padding: 10px 20px;
@@ -53,7 +53,7 @@ def get_custom_css_page(alignment="center", button_span="full"):
         }}
         
         .stButton button:hover {{
-            background-color: #4A90E2;
+            background-color: #A7D8F0;
         }}
         
         /* Align buttons (left or center) */
